e315812ea027ac4449ce70521cc0c8f2cd8d980f
ferdousanam <ferdous.anam@gmail.com>
2020-01-27 23:48:12 +0600

    Migration files created & Group table created

---
+++ database/migrations/2020_01_27_173921_create_groups_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('group_name');
+            $table->string('leader_name')->nullable();
+            $table->tinyInteger('management_type')->comment('0=Public | 1=Private');
+            $table->text('address')->nullable();
+            $table->string('contact_no')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
