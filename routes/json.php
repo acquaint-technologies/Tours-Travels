5e8c23b6c2bd4e1e18839f46c2a2f4c1bfd23164
ferdousanam <ferdous.anam@gmail.com>
2020-02-09 02:57:29 +0600

    Get Thana List from district

---
+++ routes/json.php
@@ -0,0 +1,17 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| JSON Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group.
+|
+| Response should be JSON datatype
+|
+*/
+
+Route::group(['prefix' => 'json', 'namespace' => 'JsonCon', 'middleware' => ['auth:admin']], function () {
+    Route::get('get-upazilas/{district}', 'LocationController@getUpazilasFromDistrict');
+});
