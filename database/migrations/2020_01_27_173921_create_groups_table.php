37123f65a69c327035a7ade3b079e37c1b66fa76
ferdousanam <ferdous.anam@gmail.com>
2020-02-10 22:34:55 +0600

    groups table migration change

--- database/migrations/2020_01_27_173921_create_groups_table.php
+++ database/migrations/2020_01_27_173921_create_groups_table.php
@@ -15,10 +15,12 @@ class CreateGroupsTable extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->tinyInteger('group_type')->default(1)->comment('1=Hajj | 2=Omra Hajj');
             $table->string('group_name');
             $table->string('group_code')->unique();
             $table->string('leader_name')->nullable();
-            $table->tinyInteger('management_type')->comment('0=Public | 1=Private');
+            $table->tinyInteger('management_type')->comment('0=Group Leader | 1=Office');
+            $table->string('management_name')->nullable();
             $table->text('address')->nullable();
             $table->string('contact_no')->nullable();
             $table->string('email')->nullable();
