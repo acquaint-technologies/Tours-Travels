c37d44df9c0be783cce7d246aec531e2d9368788
Ferdous Anam <ferdous.anam@gmail.com>
2020-03-17 14:38:22 +0600

    Delete Passport from Passport Collection

--- resources/views/Admin/passport-collection/form.blade.php
+++ resources/views/Admin/passport-collection/form.blade.php
@@ -8,6 +8,7 @@
 @endif
 
 @section('content')
+    @include('dashboard::components.delete-modal')
     @include('dashboard::msg.message')
     <!--begin::Portlet-->
     <div class="kt-portlet">
@@ -116,50 +117,59 @@
 
                 @if (isset($passport_collection->id))
                     @foreach($passport_collection->submitted_passports as $passport)
-                        <input type="hidden" name="passport_id[]" value="{{ $passport->id }}">
                         <div class="card mt-3" id="passport-{{ $passport->id }}">
+                            <input type="hidden" name="passport_id[]" value="{{ $passport->id }}">
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-6">
-                                        <div class="form-group row">
-                                            <label for="full_name[]" class="col-3 col-form-label">
-                                                Full Name *
-                                            </label>
-                                            <div class="col-9">
-                                                <input class="form-control" type="text" id="full_name[]" name="passport[full_name][]"
-                                                       value="{{ old('full_name[]', $passport->full_name) }}" placeholder="Full Name" required>
+                                    <div class="col-10">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="form-group row">
+                                                    <label for="full_name[]" class="col-3 col-form-label">
+                                                        Full Name *
+                                                    </label>
+                                                    <div class="col-9">
+                                                        <input class="form-control" type="text" id="full_name[]" name="passport[full_name][]"
+                                                               value="{{ old('full_name[]', $passport->full_name) }}" placeholder="Full Name" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <div class="form-group row">
+                                                    <label for="passport_no[]" class="col-3 col-form-label">
+                                                        Passport Number *
+                                                    </label>
+                                                    <div class="col-9">
+                                                        <input class="form-control" type="text" id="passport_no[]" name="passport[passport_no][]"
+                                                               value="{{ old('passport_no[]', $passport->passport_no) }}" placeholder="Passport Number" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <div class="form-group row">
+                                                    <label for="date_of_birth[]" class="col-3 col-form-label">Date Of Birth *</label>
+                                                    <div class="col-9">
+                                                        <input class="form-control kt-datepicker" type="text" id="date_of_birth[]" name="passport[date_of_birth][]"
+                                                               value="{{ \Carbon\Carbon::parse(old('date_of_birth[]', $passport->date_of_birth))->format('d-m-Y') }}" placeholder="Date Of Birth" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <div class="form-group row">
+                                                    <label for="expiry_date[]" class="col-3 col-form-label">Expiry Date *</label>
+                                                    <div class="col-9">
+                                                        <input class="form-control kt-datepicker" type="text" id="expiry_date[]" name="passport[expiry_date][]"
+                                                               value="{{ \Carbon\Carbon::parse(old('expiry_date[]', $passport->expiry_date))->format('d-m-Y') }}" placeholder="" required>
+                                                        <span class="form-text text-danger" id="calculated_passport_expiry"></span>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-6">
-                                        <div class="form-group row">
-                                            <label for="passport_no[]" class="col-3 col-form-label">
-                                                Passport Number *
-                                            </label>
-                                            <div class="col-9">
-                                                <input class="form-control" type="text" id="passport_no[]" name="passport[passport_no][]"
-                                                       value="{{ old('passport_no[]', $passport->passport_no) }}" placeholder="Passport Number" required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-6">
-                                        <div class="form-group row">
-                                            <label for="date_of_birth[]" class="col-3 col-form-label">Date Of Birth *</label>
-                                            <div class="col-9">
-                                                <input class="form-control kt-datepicker" type="text" id="date_of_birth[]" name="passport[date_of_birth][]"
-                                                       value="{{ \Carbon\Carbon::parse(old('date_of_birth[]', $passport->date_of_birth))->format('d-m-Y') }}" placeholder="Date Of Birth" required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-6">
-                                        <div class="form-group row">
-                                            <label for="expiry_date[]" class="col-3 col-form-label">Expiry Date *</label>
-                                            <div class="col-9">
-                                                <input class="form-control kt-datepicker" type="text" id="expiry_date[]" name="passport[expiry_date][]"
-                                                       value="{{ \Carbon\Carbon::parse(old('expiry_date[]', $passport->expiry_date))->format('d-m-Y') }}" placeholder="" required>
-                                                <span class="form-text text-danger" id="calculated_passport_expiry"></span>
-                                            </div>
-                                        </div>
+                                    <div class="col-2 text-center">
+                                        <button type="button" class="btn btn-danger btn-sm btn-icon-sm btn-circle delete-button" data-toggle="modal" data-target="#delete-modal" data-id="{{ $passport->id }}">
+                                            <i class="flaticon-delete"></i>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
@@ -191,6 +201,7 @@
 @endsection
 
 @push('scripts')
+    @include('Admin.passport-collection.delete')
     <script>
         $(document).ready(function () {
             $('#passport-management-mm').addClass('kt-menu__item--submenu kt-menu__item--open kt-menu__item--here');
